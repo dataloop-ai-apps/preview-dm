02c74743d52e763be2cca5a2b1e5084598f4175f
ludoHorv <ludo.h@dataloop.ai>
2025-07-18 16:48:46 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,134 @@
+<template>
+    <dl-theme-provider :is-dark="isDark">
+        <div class="full-screen">
+            <div v-if="loading" class="loading-spinner">
+                <dl-spinner text="Loading App..." size="100px" type="grid" />
+            </div>
+
+            <div v-else>
+                <div v-if="message" class="message">
+                    {{ message }}
+                </div>
+                <FloatingWindow
+                    v-if="!message"
+                    :loading="loading"
+                    :type-of-content="typeOfContent"
+                    :set-is-open="(value) => (isOpen = value)"
+                    :width="initialWidth"
+                    :height="initialHeight"
+                    :url="url"
+                    :type="type"
+                />
+            </div>
+        </div>
+    </dl-theme-provider>
+</template>
+
+<script setup>
+import { ref, onMounted, computed, nextTick } from 'vue-demi'
+import FloatingWindow from './components/FloatingWindow.vue'
+import { DlEvent, ThemeType } from '@dataloop-ai/jssdk'
+import { DlThemeProvider, DlSpinner } from '@dataloop-ai/components'
+
+defineProps({
+    msg: {
+        type: String,
+        default: ''
+    }
+})
+
+const isOpen = ref(false)
+const loading = ref(true)
+const typeOfContent = ref('image')
+const initialWidth = null
+const initialHeight = null
+const type = ref('image/png')
+const url = ref('')
+const currentTheme = ref(ThemeType.LIGHT)
+const message = ref('')
+
+const isDark = computed(() => currentTheme.value === ThemeType.DARK)
+
+onMounted(async () => {
+    console.log('onMounted')
+    await dl.init()
+    dl.on(DlEvent.READY, async () => {
+        const settings = await window.dl.settings.get()
+        currentTheme.value = settings.theme
+        dl.on(DlEvent.THEME, (data) => {
+            currentTheme.value = data
+        })
+        dl.on('items:selection:updated', async (data) => {
+            console.log('SELECTION_UPDATED', data)
+            message.value = ''
+            if (data.length === 0) {
+                message.value =
+                    'Please select a single item without using the Select All option.'
+            } else if (data.length > 1) {
+                message.value =
+                    'Please select a single item without using the Select All option.'
+            } else {
+                await setItem(data[0])
+            }
+            loading.value = false
+        })
+    })
+})
+
+const setItem = async (itemId) => {
+    loading.value = true
+    const item = await dl.items.get(itemId, {
+        timeout: 10000
+    })
+    const mimetype = item.metadata.system.mimetype
+    // Supported mimetypes based on FloatingWindow.vue
+    const supported = [
+        'video', // any video/*
+        'image', // any image/*
+        'pdf', // application/pdf
+        'audio', // any audio/*
+        'json', // application/json
+        'plain', // text/plain
+        'txt' // text/txt or similar
+    ]
+    // Check if mimetype is supported
+    const isSupported = supported.some((type) => mimetype.includes(type))
+    if (!isSupported) {
+        message.value = `This mimetype is not supported: ${mimetype}`
+        loading.value = false
+        url.value = ''
+        type.value = ''
+        typeOfContent.value = ''
+        return
+    }
+    url.value = await dl.items.stream(item.stream, {
+        timeout: 10000
+    })
+    type.value = mimetype
+    typeOfContent.value = mimetype
+    // if mimetype is not supported, show a message
+}
+</script>
+
+<style scoped>
+.loading-spinner {
+    display: grid;
+    place-items: center;
+    height: 100vh;
+    background-color: var(--dl-color-studio-panel);
+    color: var(--dl-color-darker);
+}
+.content {
+    display: grid;
+    width: 100%;
+    height: 100%;
+    place-items: center;
+}
+.message {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: var(--dl-color-darker);
+}
+</style>
---
+++ src/components/PDFViewer/PDFViewer.vue
@@ -0,0 +1,95 @@
+<template>
+    <div id="pdf-document" class="relative flex flex-col h-screen">
+        <PDFNavbar
+            :title="getFilenameFromUrl(url)"
+            :num-pages="pdf?.numPages"
+            :show-sidebar="showSidebar"
+            :page-scale-value="pageScaleValue"
+            :page-zoom="pageZoom"
+            @on-toggle-sidebar="toggleSidebar"
+            @on-page-prev="toRaw(pdfViewer)?.previousPage()"
+            @on-page-next="toRaw(pdfViewer)?.nextPage()"
+            @on-zoom-in="toRaw(pdfViewer)?.increaseScale()"
+            @on-zoom-out="toRaw(pdfViewer)?.decreaseScale()"
+            @on-page-scale-change="setPageScale"
+        />
+        <PDFDocument
+            v-if="pdf"
+            :show-sidebar="showSidebar"
+            :page-scale-value="pageScaleValue"
+            @on-pdf-viewer-set="setPdfViewer"
+            style="max-width: 100%"
+        />
+        <div
+            v-else
+            class="font-thin text-2xl text-center text-gray-800 h-40 m-auto"
+        >
+            Loading...
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, provide, ref, toRaw } from 'vue'
+import * as pdfjs from 'pdfjs-dist'
+import { PDFDocumentProxy, getFilenameFromUrl } from 'pdfjs-dist'
+import {
+    PDFViewer,
+    EventBus,
+    PDFLinkService,
+    PDFFindController
+} from 'pdfjs-dist/web/pdf_viewer'
+
+pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`
+
+const props = defineProps({
+    url: {
+        type: String,
+        required: true
+    }
+})
+
+const pageScaleValue = ref('page-width')
+const pageZoom = ref(1)
+const pdf = ref<PDFDocumentProxy | null>(null)
+const showSidebar = ref(true)
+const pdfViewer = ref<PDFViewer>(null)
+
+const setPdfViewer = (viewer: PDFViewer) => {
+    pdfViewer.value = viewer
+}
+
+const setPageScale = (fit: string) => {
+    pageScaleValue.value = fit
+}
+
+const eventBus = new EventBus()
+const pdfLinkService = new PDFLinkService({ eventBus })
+const pdfFindController = new PDFFindController({
+    eventBus,
+    linkService: pdfLinkService
+})
+
+provide('pdf', pdf)
+provide('eventBus', eventBus)
+provide('pdfLinkService', pdfLinkService)
+provide('pdfFindController', pdfFindController)
+
+onMounted(async () => {
+    const loadingTask = pdfjs.getDocument({
+        url: props.url,
+        cMapUrl: `https://unpkg.com/pdfjs-dist@${pdfjs.version}/cmaps/`,
+        cMapPacked: true,
+        enableXfa: true
+    })
+
+    const pdfDocument = await loadingTask.promise
+    pdf.value = pdfDocument
+
+    eventBus.on('scalechanging', ({ scale }) => {
+        pageZoom.value = scale
+    })
+})
+
+const toggleSidebar = () => (showSidebar.value = !showSidebar.value)
+</script>
